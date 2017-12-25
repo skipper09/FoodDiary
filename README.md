4fb2f41e2c1ebb583dad2cec0c411b5f35baa9e1
Sarah Payne <sarah@Sarahs-MacBook-Pro.local>
2017-12-25 09:54:09 -0600

    basis server set up

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # FoodDiary
 A food diary for logging daily intake that uses USDA's db to calculate nutrient totals for the day.
+
+I often tell my patients to log their daily food intake and then go back and calculate their nutrient totals using various websites that provide a nice GUI to USDA's nutrient database.
+
+I thought I would make a food diary that automatically calculates nutrient totals based on what a user enters for the day.
+
+
---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express'),
+    bodyParser = require('body-parser'),
+    methodOverride = require('method-override'),
+    mysql = require('mysql'),
+    path = require('path');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use(methodOverride('_method'));
+
+// ROUTES ///////////////////////////////////////////////////
+
+app.get('/', function(req,res) {
+    res.sendFile(path.join(__dirname,'index.html'));
+});
+
+
+
+// SERVER CONNECTION ///////////////////////////////////////////////////
+const port = 3000;
+app.listen(port);
+
+const connection = mysql.createConnection({
+    host:'localhost',
+    user: 'root',
+    password: 'password',
+    database: 'fooddiary_db'
+})
+
+connection.connect(function(err){
+    if (err) throw err;
+    console.log('Connected as id: ' + connection.threadId)
+})
